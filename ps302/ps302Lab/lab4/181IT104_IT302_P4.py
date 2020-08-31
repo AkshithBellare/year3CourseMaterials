267867be2b075370086b80e15b46d883b9485840
AkshithBellare <akshithnm@gmail.com>
2020-08-31 12:11:12 +0530

    ps302Lab: Updated program

--- ps302/ps302Lab/lab4/181IT104_IT302_P4.py
+++ ps302/ps302Lab/lab4/181IT104_IT302_P4.py
@@ -11,13 +11,16 @@ def nCr(n, r):
 
 def getInput(message, var, res_str):
     try:
-        val = int(input(message)) 
+        val = int(input("Enter the " + message)) 
         if(val < 0):
             res_str += "Invalid input: " + val + " entered for " + var + " was not a positive integer"
             res_str += "\nPlease enter a positive integer for " + var 
             outFile.write(res_str)
             print(res_str)
             exit()
+        res_str += message + str(val) + "\n"
+        outFile.write(res_str)
+        print(res_str)
     except:
         res_str += "Invalid input: input for " + var + " was not a non-integer\nPlease enter positive integer"
         outFile.write(res_str)
@@ -25,40 +28,34 @@ def getInput(message, var, res_str):
         exit()
     return val
     
-N = getInput("Enter the number of pens to be selected: N = ", "N", res_str) 
-X = getInput("Enter the number of Blue pens : X = ", "N", res_str) 
-Y = getInput("Enter the number of Red pens : Y = ", "Y", res_str)
-Z = getInput("Enter the number of Green pens: Z = ", "Z", res_str)
+N = getInput("number of pens to be selected: N = ", "N", res_str) 
+X = getInput("number of Blue pens : X = ", "N", res_str) 
+Y = getInput("number of Red pens : Y = ", "Y", res_str)
+Z = getInput("number of Green pens: Z = ", "Z", res_str)
 if(N > (X + Y + Z )):
-    print("N has to be lesser than total pens")
+    res_str+="N has to be lesser than total pens\n"
+    outFile.write(res_str)
+    print(res_str)
+    exit()
 
 den = nCr(X+Y+Z, N)
 col_sum = [int('0')]*(N+1)
+row_sum = [int('0')]*(N+1)
 res_str += "X/Y\t" 
-print("X/Y\t", end="")
 for i in range(N+1):
     res_str += str(i) + "\t"
-    print("{}\t".format(i), end="")
 res_str += "h(y)\n"
-print("h(y)\n")
 for i in range(N+1):
     res_str += str(i) + "\t"
-    print(i, end="\t")
-    sum = 0
     for j in range(N+1):
         num = nCr(X,i) * nCr(Y, j) * nCr(Z, N - i - j)
-        col_sum[j]+=num
-        sum = sum + num
+        col_sum[j] += num
+        row_sum[i] += num
         res_str+= str(num) + "/" + str(den) + "\t"
-        print("{}/{}".format(num, den), end="\t")
-    res_str += str(sum) + "/" + str(den) + "\n"
-    print("{}/{}\n".format(sum, den))
+    res_str += str(row_sum[i]) + "/" + str(den) + "\n"
 res_str += "g(x)\t"
-print("g(x)\t",end="")
 for i in range(N+1):
     res_str+=str(col_sum[i]) + "/" + str(den) + "\t"
-    print("{}/{}".format(col_sum[i],den), end="\t")
-print("\n")
 res_str += "\n"
 print(res_str)
-print("\n")
+outFile.write(res_str)
