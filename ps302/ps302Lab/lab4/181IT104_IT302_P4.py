57615f95456bacb1041403a41b7bbdf676aeb2a1
AkshithBellare <akshithnm@gmail.com>
2020-08-31 12:21:09 +0530

    ps302Lab: Lab program 4

--- ps302/ps302Lab/lab4/181IT104_IT302_P4.py
+++ ps302/ps302Lab/lab4/181IT104_IT302_P4.py
@@ -1,5 +1,6 @@
 outFile = open("output.txt", "w")
 res_str = ""
+
 def fact(n):
     res = 1
     for i in range(2, n+1):
@@ -32,6 +33,7 @@ N = getInput("number of pens to be selected: N = ", "N", res_str)
 X = getInput("number of Blue pens : X = ", "N", res_str) 
 Y = getInput("number of Red pens : Y = ", "Y", res_str)
 Z = getInput("number of Green pens: Z = ", "Z", res_str)
+
 if(N > (X + Y + Z )):
     res_str+="N has to be lesser than total pens\n"
     outFile.write(res_str)
@@ -41,21 +43,22 @@ if(N > (X + Y + Z )):
 den = nCr(X+Y+Z, N)
 col_sum = [int('0')]*(N+1)
 row_sum = [int('0')]*(N+1)
-res_str += "X/Y\t" 
+res_str += "X/Y\t\t" 
 for i in range(N+1):
-    res_str += str(i) + "\t"
+    res_str += str(i) + "\t\t"
 res_str += "h(y)\n"
 for i in range(N+1):
-    res_str += str(i) + "\t"
+    res_str += str(i) + "\t\t"
     for j in range(N+1):
         num = nCr(X,i) * nCr(Y, j) * nCr(Z, N - i - j)
         col_sum[j] += num
         row_sum[i] += num
-        res_str+= str(num) + "/" + str(den) + "\t"
+        res_str+= str(num) + "/" + str(den) + "\t\t"
     res_str += str(row_sum[i]) + "/" + str(den) + "\n"
-res_str += "g(x)\t"
+res_str += "g(x)\t\t"
 for i in range(N+1):
-    res_str+=str(col_sum[i]) + "/" + str(den) + "\t"
+    res_str+=str(col_sum[i]) + "/" + str(den) + "\t\t"
 res_str += "\n"
 print(res_str)
 outFile.write(res_str)
+
