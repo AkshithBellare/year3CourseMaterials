4f90819e2a8ab455f52b3a73ce9c745c810ad7b4
AkshithBellare <akshithnm@gmail.com>
2020-09-05 22:37:01 +0530

    pc301Lab

---
+++ pc301/pc301Lab/lab4/program1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<omp.h>
+
+int fibo(int n);
+
+int main() {
+    int n;
+    int fib;
+    double st, et;
+    printf("Enter the value of n:\n");
+    scanf("%d", &n);
+    st = omp_get_wtime();
+
+    #pragma omp parallel shared(n)
+    {
+        #pragma omp single
+        {
+            fib = fibo(n);
+        }
+    }
+    et = omp_get_wtime();
+    printf("Fib is %d\n", fib);
+    printf("Time taken is %f s\n", et - st);
+}
+
+int fibo(int n) {
+    int a, b;
+    if(n < 2) {
+        return n;
+    }
+    else {
+        #pragma omp task shared(a) if(n > 5)
+        {
+            printf("Task created by thread %d\n n is %d\n", omp_get_thread_num(), n);
+            a = fibo(n-1);
+            printf("Thread executed by thread %d\ta=%d\n", omp_get_thread_num(), a);
+        }
+
+        #pragma omp task shared(b) if(n>5)
+        {
+            printf("Task created by thread %d\n", omp_get_thread_num());
+            b = fibo(n-2);
+            printf("Task executed by thread %d\tb=%d\n",omp_get_thread_num(),b);
+        }
+
+        #pragma omp taskwait
+        {
+            return a + b;
+        } 
+    }
+}
--- pc301/pc301Lab/practice/areaPragmaFor.c
+++ pc301/pc301Lab/practice/areaPragmaFor.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<omp.h>
-static long num_steps = 1000;
+static long num_steps = 160;
 double step;
+
 int main() {
    int i; double x, pi,sum=0.0;
    
@@ -9,33 +10,33 @@ int main() {
    double start = omp_get_wtime();
    omp_sched_t kind;
    int chunk_size;
-   omp_set_schedule(omp_sched_static, 4);
-   omp_get_schedule(&kind, &chunk_size);
-   switch(kind) {
-        case omp_sched_static:
-            printf("Static");
-            break;
-        case omp_sched_dynamic:
-            printf("Dynamic");
-            break;
-        case omp_sched_guided:
-            printf("Guided");
-            break;
-        case omp_sched_auto:
-            printf("Auto");
-            break;
-        default:
-            printf("Other");
-            break;
-   }
+   omp_set_schedule(omp_sched_guided,0);
+   //switch(kind) {
+   //     case omp_sched_static:
+   //         printf("Static chunk_size = %d\n", (int)chunk_size);
+   //         break;
+   //     case omp_sched_dynamic:
+   //         printf("Dynamic chunk_size = %d\n", (int)chunk_size);
+   //         break;
+   //     case omp_sched_guided:
+   //         printf("Guided");
+   //         break;
+   //     case omp_sched_auto:
+   //         printf("Auto");
+   //         break;
+   //     default:
+   //         printf("Other");
+   //         break;
+   //}
    #pragma omp parallel for reduction(+: sum) schedule(runtime)
    for(i=0; i<num_steps; ++i) {
-       printf("%d---%d", omp_get_thread_num(), i);
        x = (i + 0.5)*step;
        sum = sum + 4.0/(1.0 + x*x);
-       printf("\n");
    }
+
+   omp_get_schedule(&kind, &chunk_size);
    double end = omp_get_wtime();
+   printf("schedule=%d chunksize=%d\n",kind,chunk_size);
    printf("final time: %f\n", end-start);
    printf("%f", step * sum);
    return 0;
